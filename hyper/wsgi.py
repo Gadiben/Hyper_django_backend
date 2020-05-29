b25ae4ccd73c207dea3d90566fa7f6e76a6f3494
Marina Menghetti <gautier_dervaux@hotmail.com>
2020-05-29 16:27:01 +0200

    fix install

--- hyper/wsgi.py
+++ hyper/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hyper_backend project.
+WSGI config for hyper project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hyper_backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hyper.settings")
 
 application = get_wsgi_application()
