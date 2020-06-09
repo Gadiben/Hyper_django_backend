fcea20c3e802f623e4a3adbd52555b6f5c4c4dae
Marina Menghetti <gautier_dervaux@hotmail.com>
2020-06-09 17:56:18 +0200

    refacto test

--- backend/endpoints/tests/test_user_endpoint.py
+++ backend/endpoints/tests/test_user_endpoint.py
@@ -18,38 +18,26 @@ class UserEndpointTests(TestCase):
         user_info_2 = dict(pseudo=username+"2",password=password,gender='male',date_of_birth='2001-01-01',longitude=0,latitude=0)
         self.user_1 = UserService.create(**user_info_1)
         self.user_2 = UserService.create(**user_info_2)
+        
+    
+    def retrive_profil_test(self, user, user_profil, response_code):
+        factory = APIRequestFactory()
+        request = factory.get("")
+        view = UserViewSet.as_view({'get': 'retrieve'})
+        force_authenticate(request, user=user.auth_id)
+        response = view(request, pk=user_profil.auth_id.id)
+
+        self.assertEqual(response.status_code, response_code)
 
 
     def test_view_own_profile(self):
         """
         Check if not logged in user 
         """
-        
-        factory = APIRequestFactory()
-        request = factory.get("")
-        
-        view = UserViewSet.as_view({'get': 'retrieve'})
-
-        # Make an authenticated request to the view...
-        force_authenticate(request, user=self.user_1.auth_id)
-        response = view(request,pk=self.user_1.auth_id.id)
-        # print("response",response.data)
-        
-        self.assertEqual(response.status_code, 200)
+        self.retrive_profil_test(self.user_1, self.user_1, 200)        
     
     def test_view_different_profile(self):
         """
         Check if not logged in user 
         """
-        
-        factory = APIRequestFactory()
-        request = factory.get("")
-        
-        view = UserViewSet.as_view({'get': 'retrieve'})
-
-        # Make an authenticated request to the view...
-        force_authenticate(request, user=self.user_2.auth_id)
-        response = view(request,pk=self.user_1.auth_id.id)
-        # print("response",response.data)
-        
-        self.assertEqual(response.status_code, 403)
+        self.retrive_profil_test(self.user_2, self.user_1, 403)
